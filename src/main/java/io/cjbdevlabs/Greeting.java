378540a741cd664fa35bc2ba3cc3374122ae2be9
Chris Bille <cbille1@icloud.com>
2025-07-02 19:37:13 -0400

    Updates to testing

--- src/main/java/io/cjbdevlabs/Greeting.java
+++ src/main/java/io/cjbdevlabs/Greeting.java
@@ -3,14 +3,23 @@ package io.cjbdevlabs;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/greeting")
 public class Greeting {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("name")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String name(@QueryParam("name") String name) {
+        return String.format("Hello there %s", name);
     }
 }
